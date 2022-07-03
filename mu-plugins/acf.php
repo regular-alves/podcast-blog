56d530ee0d637a97783eb1589784ba629b9b3ab1
Rodrigo Alves <rodrigo.gois@live.com>
2022-07-03 15:35:10 -0300

    added: must use plugins

---
+++ mu-plugins/acf.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Advanced Custom Fields
+Plugin URI: https://www.advancedcustomfields.com
+Description: Customize WordPress with powerful, professional and intuitive fields.
+Version: 5.12.2
+Author: Delicious Brains
+Author URI: https://www.advancedcustomfields.com
+Text Domain: acf
+Domain Path: /lang
+*/
+
+require WPMU_PLUGIN_DIR . '/advanced-custom-fields/acf.php';
